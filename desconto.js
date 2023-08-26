c65767756e2fd4e15817de389ffa400de4e260a8
Natália <nataliasenasantos@gmail.com>
2023-08-26 20:53:38 -0300

    correções

--- desconto.js
+++ desconto.js
@@ -1,18 +1,20 @@
-const produtos = [89, 147, 28, 10]
+const produtos = [89, 147, 56, 66, 21]
 let menorValor = produtos[0]
 let valorFinal = 0
 let valorComDesconto = 0
 let desconto = false
+
+for (let valor of produtos) {
+    if (valor < menorValor) {
+    menorValor = valor}
+}
 
 for (let i = 0; i < produtos.length; i++) {
     if (produtos.length >= 5) {
         valorFinal += produtos[i];
         valorComDesconto = valorFinal - menorValor;
         desconto = true
-        if (produtos[i] < menorValor) {
-            menorValor = produtos[i];
-        }
-    } else {
+        } else {
         valorFinal += produtos[i];
     }
 }
